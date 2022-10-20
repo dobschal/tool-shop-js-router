9be5d745301668d2f72c1c7f57726d58a3c88e79
Sascha Dobschal <s.dobschal@tdsoftware.de>
2022-10-20 08:18:38 +0200

    add hooks and made layouts optional

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 /**
- * @typedef {{ [string]: { layout: HTMLElement, page: HTMLElement }}} RouterConfig
+ * @typedef {Object} RouterConfig
+ * @property {{ [string]: { layout: HTMLElement, page: HTMLElement }}} hooks
+ * @property {{ [string]: { layout: HTMLElement, page: HTMLElement }}} routes
  */
 import Navigo from "navigo";
 
 export { RouterLink } from "./RouterLink.js";
 
-let activeLayout;
+let activeLayout, activePage;
 
 /**
  * Applies some special route handling and returns an instance of the Navigo router.
@@ -14,7 +16,10 @@ let activeLayout;
  */
 export function Router(config) {
     const router = new Navigo("/");
-    router.on(_parseConfig(config));
+    if ("hooks" in config) {
+        router.hooks(config.hooks);
+    }
+    router.on(_parseRoutesConfig(config.routes));
     router.resolve();
     return router;
 }
@@ -24,7 +29,7 @@ export function Router(config) {
  * @param {RouterConfig} config 
  * @returns {Object}
  */
-export function _parseConfig(config) {
+export function _parseRoutesConfig(config) {
     const parsedConfig = {};
     Object.keys(config).forEach(path => {
         parsedConfig[path] = () => {
@@ -37,13 +42,34 @@ export function _parseConfig(config) {
 /**
  * Switch the current shown page to another one inside a given layout.
  * The layout remains if it is the same as before.
- * @param {HTMLElement} layout - the page is rendered into
+ * @param {HTMLElement} [layout] - the page is rendered into, if not set, the page is added to the body directly
  * @param {HTMLElement} page - the page root element to exchange
  */
 export function _switchPage(layout, page) {
+
+    // Remove current active page
+    if (activePage instanceof HTMLElement) {
+        activePage.parentNode.removeChild(activePage);
+    }
+    activePage = page;
+
+    // If no layout is set, add page to body directly
+    // and remove potentially existing rendered layout
+    if (typeof layout === "undefined") {
+        if (activeLayout instanceof HTMLElement) {
+            activeLayout.parentNode.removeChild(activeLayout);
+        }
+        return document.body.append(page);
+    }
+
+    // If layout is set, check if it is already rendered,
+    // if not, remove the old one and add the new one
+    // add the page to the given "page" slot
     if (layout !== activeLayout) {
+        if (activeLayout instanceof HTMLElement) {
+            activeLayout.parentNode.removeChild(activeLayout);
+        }
         activeLayout = layout;
-        document.body.innerHTML = "";
         document.body.append(activeLayout);
     }
     layout.slots.page(page);
--- src/index.test.js
+++ src/index.test.js
@@ -1,8 +1,73 @@
-import { it, expect, describe } from "@jest/globals";
+import { jest, it, expect, describe } from "@jest/globals";
+import Navigo from "navigo";
 import { Widget } from "tool-shop-js-widget";
-import { _parseConfig } from "./index.js";
+import { _parseRoutesConfig, Router } from "./index.js";
 
 describe("Router", () => {
+
+    it("should call the hook before route is resolved", () => {
+        const hookMock = jest.fn();
+        Router({
+            hooks: {
+                before(done) {
+                    hookMock();
+                    done();
+                }
+            },
+            routes: {
+                "*": {
+                    page: Widget(),
+                    layout: Widget({
+                        slot: "page"
+                    })
+                }
+            }
+        });
+        expect(hookMock).toBeCalledTimes(1);
+    });
+
+    it("should return an instance of Navigo", () => {
+        const router = Router({
+            routes: {
+                "*": {
+                    page: Widget(),
+                    layout: Widget({
+                        slot: "page"
+                    })
+                }
+            }
+        });
+        expect(router instanceof Navigo).toBeTruthy();
+    });
+
+    it("should render the page to the DOM", () => {
+        Router({
+            routes: {
+                "*": {
+                    page: Widget({
+                        text: "yeah"
+                    }),
+                    layout: Widget({
+                        slot: "page"
+                    })
+                }
+            }
+        });
+        expect(document.body.children[0].children[0].innerText).toBe("yeah");
+    });
+
+    it("should render the page without layout to the DOM", () => {
+        Router({
+            routes: {
+                "*": {
+                    page: Widget({
+                        text: "yeah"
+                    })
+                }
+            }
+        });
+        expect(document.body.children[0].innerText).toBe("yeah");
+    });
 
     it("should parse the config correctly", () => {
         const config = {
@@ -15,7 +80,7 @@ describe("Router", () => {
                 page: Widget()
             }
         };
-        const parsedConfig = _parseConfig(config);
+        const parsedConfig = _parseRoutesConfig(config);
         expect(typeof parsedConfig.about).toBe("function");
         expect(Object.keys(parsedConfig).length).toBe(2);
     });
